2381009851771b5583ffc38fd4a1b75df3c48a7a
Jeremy Tregunna <jeremy.tregunna@me.com>
2016-08-31 16:51:11 -0600

    Initial Commit

---
+++ Calibre/Calibre.h
@@ -0,0 +1,19 @@
+//
+//  Calibre.h
+//  Calibre
+//
+//  Created by Jeremy Tregunna on 8/31/16.
+//  Copyright © 2016 Greenshire, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Calibre.
+FOUNDATION_EXPORT double CalibreVersionNumber;
+
+//! Project version string for Calibre.
+FOUNDATION_EXPORT const unsigned char CalibreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Calibre/PublicHeader.h>
+
+
