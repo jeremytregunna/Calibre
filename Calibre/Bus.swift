4f8be440786d110a6be22d81d0cc3a35b4b01959
Jeremy Tregunna <jeremy@tregunna.ca>
2016-11-10 10:27:51 -0600

    adjust visibility on bus

--- Calibre/Bus.swift
+++ Calibre/Bus.swift
@@ -8,11 +8,11 @@
 
 import Foundation
 
-enum BusError: ErrorType {
+public enum BusError: ErrorType {
     case timeout
 }
 
-class Bus<T> {
+public class Bus<T> {
     private let queue: dispatch_queue_t
     private let semaphore: dispatch_semaphore_t
     private let timeoutDelta: Int64
@@ -21,7 +21,7 @@ class Bus<T> {
     /** Create a new bus.
         - parameter timeout: Read timeout in seconds. Supports subsecond values. If negative, no timeout. Default value: `-1`.
      */
-    init(timeout: Double = -1) {
+    public init(timeout: Double = -1) {
         queue        = dispatch_queue_create("co.greenshire.library.Calibre.queues.bus", DISPATCH_QUEUE_CONCURRENT)
         semaphore    = dispatch_semaphore_create(0)
         timeoutDelta = Int64(timeout < 0 ? DISPATCH_TIME_FOREVER : UInt64(timeout * Double(NSEC_PER_SEC)))
@@ -31,7 +31,7 @@ class Bus<T> {
     /** Send a value onto the bus.
         - parameter value: The value to place onto the bus.
      */
-    func send(value: T) {
+    public func send(value: T) {
         dispatch_barrier_async(queue) {
             self.stream.append(value)
             dispatch_semaphore_signal(self.semaphore)
@@ -42,7 +42,7 @@ class Bus<T> {
         - returns: The next value waiting on the bus
         - throws: `BusError.timeout` if your (optional) read timeout expires before a value is available.
      */
-    func receive() throws -> T {
+    public func receive() throws -> T {
         var result: T? = nil
         dispatch_sync(queue) {
             let timeoutOccurred = dispatch_semaphore_wait(self.semaphore, dispatch_time(DISPATCH_TIME_NOW, self.timeoutDelta))
